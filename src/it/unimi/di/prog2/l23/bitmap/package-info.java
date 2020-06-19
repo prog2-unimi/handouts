e506372aba6c10f3e00f310ba50bb293f7000ed0
Massimo Santini <massimo.santini@gmail.com>
2020-06-19 18:26:46 +0200

    Updated links to relevant branch

--- src/it/unimi/di/prog2/l23/bitmap/package-info.java
+++ src/it/unimi/di/prog2/l23/bitmap/package-info.java
@@ -1,6 +1,6 @@
 /**
  * Per una discussione del codice di questo sottopacchetto si vedano gli <a
- * href="https://github.com/prog2-unimi/handouts/blob/master/L23.md">appunti della lezione 23</a>.
+ * href="https://github.com/prog2-unimi/handouts/blob/aa1920/L23.md">appunti della lezione 23</a>.
  *
  * <p>Il codice di questo pacchetto costituisce una possibile implementazione delle specifiche
  * "ASCII Art" basata sulla nozione di bitmap.
