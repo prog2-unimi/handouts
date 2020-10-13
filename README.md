984f09fd55777819d2c00b51888ccc64d5482aae
Massimo Santini <massimo.santini@gmail.com>
2020-10-13 10:36:08 +0200

    Sources for current lecture

--- README.md
+++ README.md
@@ -16,6 +16,48 @@ nella maggior parte dei casi *privi di commenti* e *non sono corredati da alcun
 testo esplicativo*, risulteranno probabilmente incomprensibili per chi non ha
 frequentato le lezioni).
 
+## Come ottenere ed utilizzare i sorgenti sul proprio computer
+
+Potete scaricare un [archivio
+zip](https://github.com/prog2-unimi/handouts/archive/master.zip) del contenuto
+di questo repository usando il link in questa frase, oppure il bottone verde
+"Clone or download" in altro a destra nella pagina dove state leggendo questo
+`README.md`.
+
+Una volta ottenuto e scompattato l'archivio, portatevi nella cartella che avrete
+così ottenuto; per **compilare** i file usate il comando
+
+    javac -encoding UTF-8 -d dist src/it/unimi/di/prog2/*/*.java
+
+quindi per **eseguire** una classe invocate la JVM con il *classpath*
+corrispondente a `dist`, la directory dove avete compilato i file. Ad esempio,
+per eseguire la *classe* `ComputeFactorial` del *pacchetto*
+`it.unimi.di.prog2.l04` usate il comando
+
+    java -cp dist it.unimi.di.prog2.l04.ComputeFactorial
+
+potete terminare l'esecuzione usando `^C` o `^D` (ossia premendo il tasto `ctrl`
+assieme al tasto `c` o `d`). In fine, se volete **generare la documentazione**
+in formato HTML usate il comando
+
+    javadoc -d docs -private src/it/unimi/di/prog2/*/*.java
+
+e quindi aprite nel *browser* il documento `docs/index.html`; è disponibile una
+[versione precompilata della documentazione](https://prog2-unimi.github.io/handouts/).
+
+### Usare uno strumento di build automation
+
+Per automatizzare i compiti sopra illustrati, ma sottolineando che *non è
+assolutamente materia dell'insegnamento* e **non è assolutamente richiesto per
+il superamento dell'esame**, gli studenti interessati possono usare uno
+strumento di [build automation](https://en.wikipedia.org/wiki/Build_automation),
+come quello mostrato del [repository
+d'esempio](https://github.com/prog2-unimi/build-automation-example); in tal
+caso, però, si osservi che sarà  necessario *ristrutturare* la directory `src`
+contenuta in questo repository secondo l'organizzazione richiesta dal tool (e
+mostrata nel repository d'esempio).
+
 ## Il materiale dello scorso anno accademico
 
-Gli studenti che hanno frequentato il precedente anno accademico possono trovare il materiale nell'[apposito branch](../../tree/aa1920) di questo repository.
+Gli studenti che hanno frequentato il precedente anno accademico possono trovare
+il materiale nell'[apposito branch](../../tree/aa1920) di questo repository.
---
+++ src/it/unimi/di/prog2/t05/Arrays.java
@@ -0,0 +1,122 @@
+/*
+
+Copyright 2019 Massimo Santini
+
+This file is part of "Programmazione 2 @ UniMI" teaching material.
+
+This is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This material is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this file.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
+package it.unimi.di.prog2.t05;
+
+/**
+ * A class that provides a number of standalone procedures that are useful for manipulating arrays
+ * of ints.
+ */
+public class Arrays {
+
+  private Arrays() {} // so that no object of this class can be instantiated
+
+  /**
+   * Searches the specified element in the array.
+   *
+   * @param a an array.
+   * @param x the element to search.
+   * @return an index <code>i</code> such that <code>a[i] == x</code> if <code>x</code> is in <code>
+   *     a</code>, -1 otherwise.
+   */
+  public static int search(int[] a, int x) {
+    if (a == null) return -1;
+    for (int i = 0; i < a.length; i++) if (a[i] == x) return i;
+    return -1;
+  }
+
+  /**
+   * Searches the specified element in the array.
+   *
+   * @param a a <em>sorted</em> array.
+   * @param x the element to search.
+   * @return an index <code>i</code> such that <code>a[i] == x</code> if <code>x</code> is in <code>
+   *     a</code>, -1 otherwise.
+   */
+  public static int searchSorted(int[] a, int x) {
+    if (a == null) return -1;
+    int low = 0;
+    int high = a.length - 1;
+    while (low <= high) {
+      int mid = (low + high) >>> 1;
+      if (x == a[mid]) return mid;
+      if (x < a[mid]) high = mid - 1;
+      else low = mid + 1;
+    }
+    return -1;
+  }
+
+  /**
+   * Rearranges the elements of a into ascending order.
+   *
+   * <p>For example, if <code>a = [3, 1, 6, 1]</code> before the call, on return <code>
+   * a = [1, 1, 3, 6]</code>.
+   *
+   * @param a an array.
+   */
+  public static void sort(int[] a) {
+    if (a == null) return;
+    quickSort(a, 0, a.length - 1);
+  }
+
+  /**
+   * Sorts the given subarray.
+   *
+   * <p>More precisely, reorders <code>a[low], a[low+1], …, a[high]</code> in ascending order.
+   *
+   * @param a a <em>non null</em> array.
+   * @param low the lower limit, must be non negative.
+   * @param high the upper limit, must be less than the length of <code>a</code>.
+   */
+  private static void quickSort(int[] a, int low, int high) {
+    if (low >= high) return;
+    int mid = partition(a, low, high);
+    quickSort(a, low, mid);
+    quickSort(a, mid + 1, high);
+  }
+
+  /**
+   * Partitions the elements of the given subarray.
+   *
+   * <p>More precisely reorders the elements into two contiguous groups, <code>a[i],…, a[res]</code>
+   * and <code>a[res+1],…, a[j]</code>, such that each element in the second group is at least as
+   * large as each element of the first group, where <code>res</code> is the returned value.
+   *
+   * @param a a <em>non null</em> array.
+   * @param i the lower limit, must be non negative and less than <code>j</code>.
+   * @param j the upper limit, must be greater than <code>i</code> and less than the array length.
+   * @return the upper limit of the first partition.
+   */
+  private static int partition(int[] a, int i, int j) {
+    int x = a[i];
+    while (true) {
+      while (a[j] > x) j--;
+      while (a[i] < x) i++;
+      if (i < j) { // need to swap
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+        j--;
+        i++;
+      } else return j;
+    }
+  }
+}
