8af0d4dc2b37db804ca9c5c0492ac01913a05f1e
Massimo Santini <santini@di.unimi.it>
2020-12-04 15:20:54 +0100

    Fixing package reference

--- src/it/unimi/di/prog2/t13/package-info.java
+++ src/it/unimi/di/prog2/t13/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Codice relativo alla lezione 14, per maggiori dettagli si veda il <a
+ * Codice relativo alla lezione 13, per maggiori dettagli si veda il <a
  * href="https://prog2.di.unimi.it/diario">diario del corso</a>.
  */
 package it.unimi.di.prog2.t13;
