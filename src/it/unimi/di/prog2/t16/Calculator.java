a26e235dbd062e345494506554235a6b0850779d
Massimo Santini <santini@di.unimi.it>
2020-12-21 12:42:42 +0100

    Just a bit of Junit

---
+++ src/it/unimi/di/prog2/t16/Calculator.java
@@ -0,0 +1,34 @@
+/*
+
+Copyright 2020 Massimo Santini
+
+This file is part of "Programmazione 2 @ UniMI" teaching material.
+
+This is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This material is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this file.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
+package it.unimi.di.prog2.t16;
+
+/**
+ * This example is adapted from JUnit 4 <a
+ * href="https://github.com/junit-team/junit4/wiki/Getting-started">Getting Started</a> wiki.
+ */
+public class Calculator {
+  public int evaluate(String expression) {
+    int sum = 0;
+    for (String summand : expression.split("\\+")) sum += Integer.parseInt(summand);
+    return sum;
+  }
+}
