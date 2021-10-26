b4708c70e6fbf8ee781c1339ca984b15f57b482b
Massimo Santini <santini@di.unimi.it>
2021-10-26 11:37:08 +0200

    Code for T07

---
+++ src/it/unimi/di/prog2/t07/NutritionFacts.java
@@ -0,0 +1,85 @@
+/*
+
+Copyright 2021 Massimo Santini
+
+This file is part of "Programmazione 2 @ UniMI" teaching material.
+
+This is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This material is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this file.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
+package it.unimi.di.prog2.t07;
+
+/** See EJ 2.2 for a discussion of this class. */
+@SuppressWarnings("unused")
+public class NutritionFacts {
+
+  private final int servingSize;
+  private final int servings;
+  private final int calories;
+  private final int fat;
+  private final int sodium;
+  private final int carbohydrate;
+
+  public static class Builder {
+
+    // required
+    private final int servingSize;
+    private final int servings;
+
+    // optional
+    private int calories = 0;
+    private int fat = 0;
+    private int sodium = 0;
+    private int carbohydrate = 0;
+
+    public Builder(int servingSize, int servings) {
+      this.servingSize = servingSize;
+      this.servings = servings;
+    }
+
+    public Builder calories(int val) {
+      calories = val;
+      return this;
+    }
+
+    public Builder fat(int val) {
+      fat = val;
+      return this;
+    }
+
+    public Builder sodium(int val) {
+      sodium = val;
+      return this;
+    }
+
+    public Builder carbohydrate(int val) {
+      carbohydrate = val;
+      return this;
+    }
+
+    public NutritionFacts build() {
+      return new NutritionFacts(this);
+    }
+  }
+
+  private NutritionFacts(Builder builder) {
+    servingSize = builder.servingSize;
+    servings = builder.servings;
+    calories = builder.calories;
+    fat = builder.fat;
+    sodium = builder.sodium;
+    carbohydrate = builder.carbohydrate;
+  }
+}
---
+++ src/it/unimi/di/prog2/t07/Polys.java
@@ -0,0 +1,42 @@
+/*
+
+Copyright 2021 Massimo Santini
+
+This file is part of "Programmazione 2 @ UniMI" teaching material.
+
+This is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This material is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this file.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
+package it.unimi.di.prog2.t07;
+
+/** A collection of methods for {@link Poly}s. */
+public class Polys {
+
+  // See EJ 2.4
+  private Polys() {}
+
+  /**
+   * Returns the derivative of the given polynomial.
+   *
+   * @param p the polynomial to differentiate.
+   * @return the derivative of {@code p}.
+   * @throws NullPointerException if {@code p} is {@code null}.
+   */
+  public static Poly diff(Poly p) throws NullPointerException {
+    Poly q = new Poly();
+    for (int i = 1; i <= p.degree(); i++) q = q.add(new Poly(p.coeff(i) * i, i - 1));
+    return q;
+  }
+}
