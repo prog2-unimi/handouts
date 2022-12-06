03c9f11b35b36eb69e993cf04b9ac53117e03a24
Massimo Santini <santini@di.unimi.it>
2022-12-06 18:02:51 +0100

    New

---
+++ src/it/unimi/di/prog2/t12/Poly.java
@@ -0,0 +1,115 @@
+/*
+
+Copyright 2022 Massimo Santini
+
+This file is part of "Programmazione 2 @ UniMI" teaching material.
+
+This is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This material is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this file.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
+package it.unimi.di.prog2.t12;
+
+/**
+ * {@code Poly}s are immutable polynomials with integer coefficients.
+ *
+ * <p>A typical {@code Poly} is \( p = c_0 + c_1 x + c_2 x^2 + \cdots + c_n x^n \).
+ */
+public interface Poly extends Iterable<Poly.Term> {
+
+  @SuppressWarnings("serial")
+  public static class NegativeExponentException extends RuntimeException {
+    public NegativeExponentException() {
+      super();
+    }
+
+    public NegativeExponentException(String message) {
+      super(message);
+    }
+  }
+
+  public static class Term {
+
+    final int coeff, degree;
+
+    public Term(final int coeff, final int degree) {
+      if (degree < 0) throw new NegativeExponentException();
+      this.degree = degree;
+      this.coeff = coeff;
+    }
+  }
+
+  /**
+   * Returns the degree of this polynomial.
+   *
+   * @return the largest exponent with a non-zero coefficient; returns 0 if this is the zero {@code
+   *     Poly}.
+   */
+  public int degree();
+
+  /**
+   * Performs polynomial addition.
+   *
+   * <p>If \( p \) is this polynomial, returns \( p + q \).
+   *
+   * @param q the polynomial to add to this one.
+   * @return the sum among this and the given polynomial.
+   * @throws NullPointerException if {@code q} is {@code null}.
+   */
+  public Poly add(Poly q);
+
+  /**
+   * Returns the negated polynomial.
+   *
+   * <p>If \( p \) is this polynomial, returns \( -p \).
+   *
+   * @return this polynomial multiplied by \( -1 \).
+   */
+  public Poly minus();
+
+  /**
+   * Performs polynomial multiplication.
+   *
+   * <p>If \( p \) is this polynomial, returns \( p q \).
+   *
+   * @param q the polynomial to multiply by this one.
+   * @return the product among this and the given polynomial.
+   * @throws NullPointerException if {@code q} is {@code null}.
+   */
+  public Poly mul(Poly q);
+
+  /**
+   * Returns the coefficient of the term of given exponent.
+   *
+   * @param degree the exponent of the term to consider.
+   * @return the coefficient of the considered term.
+   */
+  public default int coeff(int degree) {
+    for (Poly.Term t : this) if (t.degree == degree) return t.coeff;
+    return 0;
+  }
+
+  /**
+   * Performs polynomial subtraction.
+   *
+   * <p>If \( p \) is this polynomial, returns \( p - q \).
+   *
+   * @param q the polynomial to subtract from this one.
+   * @return the subtraction among this and the given polynomial.
+   * @throws NullPointerException if {@code q} is {@code null}.
+   */
+  public default Poly sub(Poly q) {
+    return this.add(q.minus());
+  }
+}
---
+++ src/it/unimi/di/prog2/t12/SparsePoly.java
@@ -0,0 +1,140 @@
+/*
+
+Copyright 2022 Massimo Santini
+
+This file is part of "Programmazione 2 @ UniMI" teaching material.
+
+This is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This material is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this file.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
+package it.unimi.di.prog2.t12;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class SparsePoly extends AbstractPoly {
+
+  /** The list of terms. */
+  private final List<Term> terms;
+
+  // RI: terms is not null, does not contain null, terms
+  //     are in strictly increasing degree order.
+
+  /** Initializes this to be the zero polynomial, that is \( p = 0 \). */
+  public SparsePoly() {
+    super(0);
+    terms = new LinkedList<>();
+  }
+
+  /**
+   * Initializes this to be the polynomial with given terms and degree.
+   *
+   * @param terms must satisfy the RI (this is a partial method).
+   */
+  private SparsePoly(final List<Poly.Term> terms) {
+    super(terms.get(terms.size() - 1).degree);
+    this.terms = new LinkedList<>(terms);
+  }
+
+  /**
+   * Initializes this to be the polynomial \(p = cx^n\).
+   *
+   * @param coeff the coefficient.
+   * @param degree the degree.
+   * @throws NegativeExponentException if {@code n} &lt; 0.
+   */
+  public SparsePoly(int coeff, int degree) throws NegativeExponentException {
+    this(List.of(new Term(coeff, degree)));
+  }
+
+  // Methods
+
+  /**
+   * Finds the index of a term of given degree.
+   *
+   * @param terms a not {@null} list of terms in increasing degree order.
+   * @param degree the degree.
+   * @return the index of a term of given degree, or -1 if none is present.
+   */
+  private static int findByDegree(List<Poly.Term> terms, int degree) {
+    for (int i = 0; i < terms.size(); i++) {
+      int d = terms.get(i).degree;
+      if (d > degree) return -1;
+      else if (d == degree) return i;
+    }
+    return -1;
+  }
+
+  @Override
+  public int coeff(int degree) {
+    int i = findByDegree(terms, degree);
+    if (i != -1) return terms.get(i).coeff;
+    return 0;
+  }
+
+  /**
+   * Adds a term to the list.
+   *
+   * <p>The list will remain in increasing degree order, in case a term with the same degree was
+   * present, the two will be added (and removed if the coefficient will become 0).
+   *
+   * @param terms a not {@null} list of terms in increasing degree order.
+   * @param term the term.
+   */
+  private static void addTerm(List<Poly.Term> terms, Poly.Term term) {
+    if (term.coeff == 0) return;
+    int i = findByDegree(terms, term.degree);
+    if (i != -1) {
+      int c = terms.get(i).coeff + term.coeff;
+      if (c != 0) terms.set(i, new Poly.Term(c, term.degree));
+      else terms.remove(i);
+    } else {
+      for (i = 0; i < terms.size(); i++) if (terms.get(i).degree > term.degree) break;
+      terms.add(i, term);
+    }
+  }
+
+  @Override
+  public SparsePoly add(Poly q) throws NullPointerException {
+    if (q == null) throw new NullPointerException();
+    List<Poly.Term> terms = new LinkedList<>(this.terms);
+    for (Poly.Term t : q) addTerm(terms, t);
+    return new SparsePoly(terms);
+  }
+
+  @Override
+  public SparsePoly mul(Poly q) throws NullPointerException {
+    if (q == null) throw new NullPointerException();
+    List<Poly.Term> terms = new LinkedList<>();
+    for (Poly.Term tq : q)
+      for (Poly.Term tt : this)
+        addTerm(terms, new Poly.Term(tq.coeff * tt.coeff, tq.degree + tt.degree));
+    return new SparsePoly(terms);
+  }
+
+  @Override
+  public SparsePoly minus() {
+    List<Poly.Term> terms = new LinkedList<>();
+    for (Poly.Term t : this) terms.add(new Poly.Term(-t.coeff, t.degree));
+    return new SparsePoly(terms);
+  }
+
+  @Override
+  public Iterator<Term> iterator() {
+    return Collections.unmodifiableList(terms).iterator();
+  }
+}
