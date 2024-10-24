fc500534992e52694b10dab087730fbb5f0985f9
Massimo Santini <santini@di.unimi.it>
2024-10-24 10:04:29 +0200

    Solutions

---
+++ src/main/java/it/unimi/di/prog2/s06/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Soluzioni relative agli esercizi della lezione 06 per maggiori dettagli si veda il <a
+ * href="https://prog2.di.unimi.it/diario">diario del corso</a>.
+ */
+package it.unimi.di.prog2.s06;
